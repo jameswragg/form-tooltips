ae67136d10c08a68cfd2e43b8716edd7228d3f62
James Wragg <james.wragg@madgex.com>
2015-02-18 13:31:19 +0000

    Initial commit

---
+++ tooltips.js
@@ -0,0 +1,47 @@
+/**
+ * Tooltip toggler
+ */
+(function() {
+
+    var MDGX = window['MDGX'] || {};
+
+    MDGX.Tooltips = (function(){
+
+        var Tooltips = {},
+            className = 'js-hasfocus';
+
+
+        function init(){
+            console.time('Tooltips Init');
+
+            $('input, textarea').each(function(el) {
+
+                var $this = $(this),
+                    $container = $(this).parents('.field').first(),
+                    $tip = $container.find('.tooltip').eq(0),
+                    availSpace;
+
+                $this.on('focus', function(e) {
+                    availSpace = $('body').width() - ($this.offset().left + $this.outerWidth());
+
+                    // make sure the tip of the tip bubble actually sits on the top of the input
+                    $tip.css('top', $this.position().top);
+                    $container.addClass(className);
+
+                }).on('blur', function(e) {
+                    $container.removeClass(className);
+                });
+
+            });
+
+            console.timeEnd('Tooltips Init');
+        }
+
+        $(document).on('app:load-complete', init);
+
+        Tooltips.init = init;
+
+        return Tooltips;
+    })();
+
+}());
